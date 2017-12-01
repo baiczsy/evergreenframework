33d5ce8fe683845d63d08e6ab1866113a62c4d93
wangl <3147083@qq.com>
2017-12-01 22:40:35 +0800

    优化dbhelper异常处理

--- evergreenframework-dbhelper/src/main/java/org/evergreen/db/helper/BeanUtil.java
+++ evergreenframework-dbhelper/src/main/java/org/evergreen/db/helper/BeanUtil.java
@@ -1,14 +1,17 @@
 package org.evergreen.db.helper;
 
 import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.SQLXML;
 import java.sql.Timestamp;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -43,17 +46,51 @@ public class BeanUtil {
      * @return Object
      */
     public static Object createBean(ResultSet rs, Class<?> type) throws SQLException {
-        try {
-            Object bean = type.newInstance();
-            ResultSetMetaData metaData = rs.getMetaData();
+        Object bean = newInstance(type);
+        PropertyDescriptor[] propertyDescriptors = propertyDescriptors(type);
+        ResultSetMetaData metaData = rs.getMetaData();
+        for (PropertyDescriptor pd : propertyDescriptors) {
             for (int i = 1; i <= metaData.getColumnCount(); i++) {
                 String columnName = metaData.getColumnLabel(i);
-                setProperty(columnName, type, bean, rs);
+                if(callSetter(pd, columnName, type, bean, rs)) {
+                    break;
+                }
             }
-            return bean;
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new SQLException(e.getMessage());
+        }
+        return bean;
+    }
+
+    /**
+     * 构建Bean实例
+     *
+     * @param type
+     * @return
+     * @throws SQLException
+     */
+    private static Object newInstance(Class<?> type) throws SQLException {
+        try {
+            return type.newInstance();
+        } catch (InstantiationException e) {
+            throw new SQLException("Cannot create " + type.getName() + ": " + e.getMessage());
+        } catch (IllegalAccessException e) {
+            throw new SQLException("Cannot create " + type.getName() + ": " + e.getMessage());
+        }
+    }
+
+    /**
+     * 内省后去所有的属性描述
+     *
+     * @param type
+     * @return
+     * @throws SQLException
+     */
+    private static PropertyDescriptor[] propertyDescriptors(Class<?> type)
+            throws SQLException {
+        try {
+            BeanInfo beanInfo = Introspector.getBeanInfo(type, Object.class);
+            return beanInfo.getPropertyDescriptors();
+        } catch (IntrospectionException e) {
+            throw new SQLException("Bean introspection failed: " + e.getMessage());
         }
     }
 
@@ -63,12 +100,9 @@ public class BeanUtil {
      * @param columnName
      * @throws Exception
      */
-    private static void setProperty(String columnName, Class<?> type, Object bean,
-                                      ResultSet rs) throws Exception {
-        BeanInfo beanInfo = Introspector.getBeanInfo(type, Object.class);
-        PropertyDescriptor[] propertyDescriptors = beanInfo
-                .getPropertyDescriptors();
-        for (PropertyDescriptor pd : propertyDescriptors) {
+    private static boolean callSetter(PropertyDescriptor pd, String columnName, Class<?> type, Object bean,
+                                    ResultSet rs) throws SQLException {
+        try {
             type = pd.getReadMethod().getDeclaringClass();
             String fieldName = pd.getName();
             Field field = type.getDeclaredField(fieldName);
@@ -80,9 +114,16 @@ public class BeanUtil {
                     value = primitiveDefaults.get(pd.getPropertyType());
                 }
                 pd.getWriteMethod().invoke(bean, value);
-                break;
+                return true;
             }
+        } catch (NoSuchFieldException e) {
+            throw new SQLException("Cannot set " + pd.getName() + ": " + e.getMessage());
+        } catch (IllegalAccessException e) {
+            throw new SQLException("Cannot set " + pd.getName() + ": " + e.getMessage());
+        } catch (InvocationTargetException e) {
+            throw new SQLException("Cannot set " + pd.getName() + ": " + e.getMessage());
         }
+        return false;
     }
 
 }
