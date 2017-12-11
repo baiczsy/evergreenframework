9ddc1674b81e46494847d6de4c1518963d3961f3
wangl <3147083@qq.com>
2017-12-11 09:52:06 +0800

    优化beans代码

--- evergreenframework-beans/src/main/java/org/evergreen/beans/factory/DependencyInvoker.java
+++ evergreenframework-beans/src/main/java/org/evergreen/beans/factory/DependencyInvoker.java
@@ -1,6 +1,8 @@
 package org.evergreen.beans.factory;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.ServiceLoader;
 
 /**
@@ -9,21 +11,20 @@ import java.util.ServiceLoader;
  */
 public class DependencyInvoker {
 
-	/**
-	 * 使用ServiceLoader(SPI方式)初始化注入器
-	 */
-	private static ServiceLoader<InjectHandler> loaders;
+	private static List<InjectHandler> handlers = new ArrayList<>();
+
 
 	static {
-		loaders = ServiceLoader.load(InjectHandler.class);
+		Iterator<InjectHandler> iterator = ServiceLoader.load(InjectHandler.class).iterator();
+		while(iterator.hasNext()){
+			handlers.add(iterator.next());
+		}
 	}
 
 	// 执行注入操作
 	public static Object inject(Object bean, Class<?> targetClass,BeanFactory factory) {
-		Iterator<InjectHandler> iterator = loaders.iterator();
-		// 遍历ServiceLoader,取出每一个注入器执行
-		while (iterator.hasNext()) {
-			iterator.next().execute(bean, targetClass, factory);
+		for (InjectHandler handler : handlers) {
+			handler.execute(bean, targetClass, factory);
 		}
 		return bean;
 	}
